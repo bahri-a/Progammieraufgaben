07b41e06b5f23f21b7c7e7f5c4219fc6290b3495
bahri-a <bahriakcora622@gmail.com>
2025-08-13 13:05:03 +0200

    Aufgabe 1

---
+++ Aufgabe 1 13:11:25/main.dart
@@ -0,0 +1,34 @@
+void main() {
+  String gdStr = "Guten";
+
+  String dayStr = "Tag";
+  String worldStr = "Welt";
+  int d = 20;
+  String timeStr = "Zeit";
+  int seven = 7;
+  int twelve = 12;
+  String f = "!";
+  int a = 24;
+  String c = "Schöne";
+  String morning = "Morgen";
+  String S = "Heute";
+  String spa = " ";
+
+  // print(
+  //   [
+  //     gdStr,
+  //     dayStr,
+  //     worldStr,
+  //     d,
+  //     timeStr,
+  //     seven,
+  //     twelve,
+  //     f,
+  //     a,
+  //     c,
+  //     morning,
+  //     S,
+  //     spa,
+  //   ].join("  "),
+  // );
+}
