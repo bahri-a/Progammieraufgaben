34569d34a8df4633fb93791f53aaf92cefc6be26
bahri-a <bahriakcora622@gmail.com>
2025-08-20 13:13:34 +0200

    a

---
+++ Üben/Null_Operatoren.dart
@@ -0,0 +1,18 @@
+void main(List<String> args) {
+  //if-null-Operator "??"
+
+  String? user = 'Bahri';
+  String name = user ?? 'noName'; // die "??" bedeuten quasi "falls Null, dann".
+
+  // null-assignment-Operator "??="
+
+  String? name2 = 'Bahri';
+  name2 ??= 'noName'; // die "??" bedeuten quasi "falls Null, dann".
+
+  // bang-Operator "!"
+
+  String? name3 = 'Bahri';
+  String text = "Hallo" + name3!; // sagt aus: Prüfe nicht, ob es null ist.
+
+  print(name + name2 + text);
+}
---
+++ Üben/Ternary_Operator.dart
@@ -0,0 +1,8 @@
+// Ersetzt if else Bedingung bei einfachen Ausdrücken.
+
+void main(List<String> args) {
+  int num1 = 10;
+  int num2 = 15;
+  int max = (num1 > num2) ? num1 : num2;
+  print("The greatest number is $max");
+}
