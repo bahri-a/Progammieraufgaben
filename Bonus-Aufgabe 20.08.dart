7d8804b1153629989ff1b46325cf8f18c5ac4ce6
bahri-a <bahriakcora622@gmail.com>
2025-08-20 15:17:14 +0200

    a

---
+++ Bonus-Aufgabe 20.08.dart
@@ -0,0 +1,17 @@
+void main() {
+  // Liste hat garantiert immer 3 Elemente
+  List<Map<String, double?>> weatherData = [
+    {'temp': 5.3, 'rain': 0.9, 'wind': null},
+    {'temp': 4.5, 'rain': null, 'wind': 16.8},
+    {'temp': null, 'rain': 3.8, 'wind': null},
+  ];
+
+  double? x =
+      (weatherData[2]['temp']) ??
+      (weatherData[0]['wind']) ??
+      ((4.5 >= weatherData[1]['temp']!)
+          ? weatherData[1]['temp']
+          : weatherData[1]['wind']);
+
+  print(x);
+}
