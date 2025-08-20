82244f2161b85f0c125d6d3e199e20eae1bf51fb
bahri-a <bahriakcora622@gmail.com>
2025-08-20 14:57:26 +0200

    1

--- Aufgabe 20.08.dart
+++ Aufgabe 20.08.dart
@@ -14,17 +14,39 @@ void main() {
   temps.add(weatherData[1]['temp']);
   temps.add(weatherData[2]['temp']);
 
-  double summe = 0;
-  int anzahl = 0;
+  // Berechnung der Summe und Anzahl der Werte
+  double summe_temp = 0;
+  int anzahl_temp = 0;
+
+  double summe_rain = 0;
+  int anzahl_rain = 0;
+
+  double summe_wind = 0;
+  double anzahl_wind = 0;
 
   for (int i = 0; i < weatherData.length; i++) {
     if ((weatherData[i]['temp']) != null) {
-      summe += (weatherData[i]['temp'])!;
-      anzahl++;
+      summe_temp += (weatherData[i]['temp'])!;
+      anzahl_temp++;
+    }
+    if ((weatherData[i]['rain']) != null) {
+      summe_rain += (weatherData[i]['rain'])!;
+      anzahl_rain++;
+    }
+
+    if ((weatherData[i]['wind']) != null) {
+      summe_wind += (weatherData[i]['wind'])!;
+      anzahl_wind++;
     }
   }
 
   // Berechne den Durchschnitt der Temperaturen
-  double? avgTemp = summe / anzahl;
+  double? avgTemp = summe_temp / anzahl_temp;
   print('Durchschnittstemperatur: $avgTemp');
+
+  double? avgRain = summe_rain / anzahl_rain;
+  print('Durchschnittsniederschlag: $avgRain');
+
+  double? avgWind = summe_wind / anzahl_wind;
+  print('Durchschnittswindstärke: $avgWind');
 }
