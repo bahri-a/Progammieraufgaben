4715a826b31668d0c23d8674c3cea7d1562f444a
bahri-a <bahriakcora622@gmail.com>
2025-08-27 14:33:29 +0200

    a

---
+++ Aufgaben/B1_27.08.dart
@@ -0,0 +1,21 @@
+import "dart:io";
+
+void convertMinutesToTime(int minutes) {
+  int h = 0;
+  int m = minutes % 60;
+  int counter = 0;
+  for (int i = 1; i <= minutes; i++) {
+    counter++;
+    if (counter == 60) {
+      h++;
+      counter = 0;
+    }
+  }
+
+  h == 1 ? stdout.write("$h Stunde, ") : stdout.write("$h Stunden, ");
+  m == 1 ? print("$m Minute.") : print("$m Minuten.");
+}
+
+void main(List<String> args) {
+  convertMinutesToTime(61);
+}
