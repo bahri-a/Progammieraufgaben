9d1e641e50e5bb2d63aa53d80bd309c78fd8a52a
bahri-a <bahriakcora622@gmail.com>
2025-08-27 13:55:14 +0200

    a

---
+++ Aufgaben/A2_27.08.dart
@@ -0,0 +1,9 @@
+void triplePrint(String a) {
+  print(a);
+}
+
+void main(List<String> args) {
+  triplePrint("Hallo");
+  triplePrint("triple");
+  triplePrint("Print");
+}
---
+++ Aufgaben/A3_27.08.dart
@@ -0,0 +1,11 @@
+void reverseInitials(String vor, String nach) {
+  String a = vor[vor.length - 1];
+  String b = nach[nach.length - 1];
+  print(a.toUpperCase() + ". " + b.toUpperCase() + ".");
+}
+
+void main(List<String> args) {
+  reverseInitials("Bahri", "Akcora");
+  reverseInitials("Meeresbewohner", "Weißsand");
+  reverseInitials("Can", "Müller");
+}
