2e4447bf53709bc6aecff11943e51e91f1235124
bahri-a <bahriakcora622@gmail.com>
2025-08-31 21:29:51 +0200

    fertig konsolenprogramm

--- Aufgaben/Aufgaben 29.08./A1_29.08.dart
+++ Aufgaben/Aufgaben 29.08./A1_29.08.dart
@@ -1,80 +1,98 @@
 import "dart:io";
+import "dart:math";
 
-Map<int, String> toDoList = {1: "Hausaufgaben [ ]", 2: "Müll [ ]"};
+Map<int, String> toDoList = {};
 
-// Befehl: Zur Liste hinzufügen
-void add(String aufgabe) {
-  int counter = 1;
-  for (int i in toDoList.keys) {
-    counter++;
+// Befehl wird extrahiert als String
+String befehlExtrahieren(String? eingabe) {
+  eingabe = eingabe?.trim(); //Entfernt alle Leerzeichen vorne und hinten.
+  List<String>? splittedEingabe = eingabe?.split(" ");
+  String befehl = splittedEingabe![0];
+  return befehl;
+}
+
+// toDo-Aufgabe wird extrahiert als String
+String aufgabeExtrahieren(String? eingabe) {
+  eingabe = eingabe?.trim(); //Entfernt alle Leerzeichen vorne und hinten.
+  List<String>? splittedEingabe = eingabe?.split(" ");
+  splittedEingabe?.remove(splittedEingabe[0]);
+  String toDo = splittedEingabe!.join(" ").toString();
+  return toDo;
+}
+
+// Zur Liste hinzufügen
+void addToList(String eingabe) {
+  String neueAufgabe = aufgabeExtrahieren(eingabe);
+  int newKey = 0;
+  if (toDoList.isEmpty) {
+    newKey = 1;
+  } else {
+    int max = 0;
+    for (int key in toDoList.keys) {
+      key > max ? max = key : ();
+    }
+    newKey = max + 1;
   }
-  toDoList[counter] = ("$aufgabe [ ]");
-  print("Aufgabe hinzugefügt: $aufgabe");
+
+  toDoList[newKey] = neueAufgabe;
+  print("\nAufgabe hinzugefügt: $neueAufgabe\n");
 }
 
 // Befehl: To-Do-Liste anzeigen
-void anzeigen(Map map) {
+void anzeigen(Map<int, String> toDoList) {
   print("\nUnsere To-Do-Liste:\n");
-  for (int i = 1; i <= toDoList.length; i++) {
-    print("$i. ${map[i]}");
+  if (toDoList.isEmpty)
+    print("Die Liste ist leer");
+  else {
+    for (var eintrag in toDoList.entries) {
+      print("${eintrag.key}. ${eintrag.value}");
+    }
   }
   print("\n");
 }
 
-// Befehl: Aufgaben markieren
-void erledigt(int nr) {
+// Befehl: Aufgaben als erledigt markieren
+void erledigtMarkieren(int nr) {
   toDoList[nr] = toDoList[nr]!.replaceFirst("[ ]", "[x]");
   print("Abgehakt: ${toDoList[nr]}\n");
 }
 
-// Befehl: exit
-void exit() {
-  print("Tschüss!");
-  exitCode;
+int doneZahlExtrahieren(String eingabe) {
+  List<String> done = eingabe.split(" ");
+  done.remove("done");
+  int? zahl = int.tryParse(done[0]);
+  return zahl!;
 }
 
-// Verstehen der Eingabe des Users
-void eingabeVerstehen(String? eingabe) {
-  eingabe = eingabe?.trim(); //Entfernt alle Leerzeichen vorne und hinten.
-  List<String>? splittedEingabe = eingabe?.split(" ");
-  String befehl = splittedEingabe![0];
-  splittedEingabe.remove("$befehl");
-  String aufgabe = splittedEingabe.join(" ").toString();
-  print(aufgabe);
+void delete(String eingabe) {
+  int? zahl = int.tryParse(eingabe);
+  print("\nDeleted: ${toDoList[zahl]}\n");
+  toDoList.remove(zahl);
 }
 
-void main(List<String> args) {
+void main() {
   print(
     "\nHerzlich Willkommen!\n\nMöglichkeiten:\n\nadd 'Aufgabe' \t--> Um eine Aufgabe zu erstellen\nlist \t\t--> Um die To-Do-Liste anzuzeigen\ndone 'Nummer' \t--> Um eine Aufgabe als erledigt zu markieren\nexit \t\t--> Um das Programm zu beenden\n\n",
   );
 
-  stdout.write("Eingabe: ");
-  String? eingabe = stdin.readLineSync();
-  eingabeVerstehen(eingabe);
+  while (1 < 5) {
+    stdout.write("Eingabe: ");
+    String? eingabe = stdin.readLineSync();
+    String? befehl = befehlExtrahieren(eingabe);
+    String? toDo = aufgabeExtrahieren(eingabe);
+
+    if (befehl == "add") {
+      addToList(eingabe!);
+    } else if (befehl == "done") {
+      int nr = doneZahlExtrahieren(eingabe!);
+      erledigtMarkieren(nr);
+    } else if (befehl == "list") {
+      anzeigen(toDoList);
+    } else if (befehl == "delete") {
+      delete(toDo);
+    } else if (befehl == "exit") {
+      print("\nTschüss!\n");
+      exit(0);
+    }
+  }
 }
-
-
-
-
-/* UM DIE MAP ENTRIES ZU EINER LISTE ZU MACHEN
- * 
- * void main() {
-  Map<String, String> user = {
-    'name': 'Max Mustermann',
-    'stadt': 'Monheim am Rhein'
-  };
-
-  // FEHLER: user.entries ist ein Iterable und hat keinen Index [0].
-  // print(user.entries[0]); 
-
-  // LÖSUNG: Wandle das Iterable zuerst in eine Liste um.
-  var entryList = user.entries.toList();
-
-  // 'entryList' ist jetzt eine richtige Liste, die du per Index abfragen kannst.
-  var firstEntry = entryList[0]; 
-  
-  print('Schlüssel des ersten Eintrags: ${firstEntry.key}');
-  print('Wert des ersten Eintrags: ${firstEntry.value}');
-}
- * 
- */
