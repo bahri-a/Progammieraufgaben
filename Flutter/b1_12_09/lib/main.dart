3aed9bd01a370f942260e8cce5d7d69b9b1ffd2d
bahri-a <bahriakcora622@gmail.com>
2025-09-14 17:54:27 +0200

    b1_12.09.

--- Flutter/b1_12_09/lib/main.dart
+++ Flutter/b1_12_09/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
@@ -9,12 +9,51 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(child: block("A", farbe: Colors.red)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(child: block("B", farbe: Colors.green)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(child: block("C")),
+            ),
+          ],
         ),
       ),
     );
   }
+}
+
+// Block
+Widget block(String text, {Color farbe = Colors.blue}) {
+  return Container(
+    // Äußerer Container
+    color: farbe,
+    height: 120,
+    width: 120,
+    alignment: Alignment.center,
+    padding: EdgeInsets.only(left: 40, right: 40, top: 20, bottom: 20),
+    child: Container(
+      // Innerer Container
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(221, 194, 107, 107),
+        borderRadius: BorderRadius.circular(30),
+      ),
+      height: 30,
+      width: 160,
+      child: Center(
+        child: Text(text, style: TextStyle(color: const Color.fromARGB(255, 1, 80, 145), fontSize: 20)),
+      ),
+    ),
+  );
 }
