b8bc023ce3b9fab7300ebab6cb4dab19db54e11d
bahri-a <bahriakcora622@gmail.com>
2025-09-15 20:56:46 +0200

    toast_programm

--- Flutter/Tutorial/tutorial/lib/main.dart
+++ Flutter/Tutorial/tutorial/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tutorial/toast_programm.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,43 +10,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 146, 192, 229),
-          title: Text("Tutorial"),
-          leading: Icon(Icons.add_photo_alternate_rounded),
-          actions: [Padding(padding: const EdgeInsets.only(right: 16), child: Icon(Icons.settings))],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.only(top: 0),
-          child: Stack(
-            children: [
-              //!Image.asset('assets/images/toast.jpg', fit: BoxFit.cover, height: 300),
-              SizedBox(
-                height: 300,
-                child: Center(
-                  child: Text("Toast", style: TextStyle(fontSize: 40, color: const Color.fromARGB(255, 10, 43, 232))),
-                ),
-              ),
-              ListTile(
-                tileColor: const Color.fromARGB(255, 24, 1, 108),
-                leading: Icon(Icons.abc_rounded, color: const Color.fromARGB(255, 245, 55, 42), size: 65),
-                title: Text(
-                  textAlign: TextAlign.start,
-                  "    Monheim",
-                  style: TextStyle(fontSize: 30, color: Color.fromARGB(255, 245, 55, 42), fontWeight: FontWeight.w800),
-                ),
-                trailing: Icon(Icons.play_circle, color: Colors.redAccent),
-                onTap: () {
-                  print("Hakkii");
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: toast_programm());
   }
 }
---
+++ Flutter/Tutorial/tutorial/lib/toast_programm.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class toast_programm extends StatelessWidget {
+  const toast_programm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 146, 192, 229),
+        title: Text("Tutorial"),
+        leading: Icon(Icons.add_photo_alternate_rounded),
+        actions: [Padding(padding: const EdgeInsets.only(right: 16), child: Icon(Icons.settings))],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 0),
+        child: Stack(
+          children: [
+            //!Image.asset('assets/images/toast.jpg', fit: BoxFit.cover, height: 300),
+            SizedBox(
+              height: 300,
+              child: Center(
+                child: Text("Toast", style: TextStyle(fontSize: 40, color: const Color.fromARGB(255, 10, 43, 232))),
+              ),
+            ),
+            ListTile(
+              tileColor: const Color.fromARGB(255, 24, 1, 108),
+              leading: Icon(Icons.abc_rounded, color: const Color.fromARGB(255, 245, 55, 42), size: 65),
+              title: Text(
+                textAlign: TextAlign.start,
+                "    Monheim",
+                style: TextStyle(fontSize: 30, color: Color.fromARGB(255, 245, 55, 42), fontWeight: FontWeight.w800),
+              ),
+              trailing: Icon(Icons.play_circle, color: Colors.redAccent),
+              onTap: () {
+                print("Hakkii");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
