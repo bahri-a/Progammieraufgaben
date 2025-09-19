fe6244997104f5fabcbe22167ddca81e050f40b5
bahri-a <bahriakcora622@gmail.com>
2025-09-19 15:19:36 +0200

    bottomBar hinzugefügt

--- Flutter/task/Projektarbeit/app_projekt/lib/firstScreen.dart
+++ Flutter/task/Projektarbeit/app_projekt/lib/firstScreen.dart
@@ -17,49 +17,60 @@ class Firstscreen extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.grey.shade300,
         appBar: AppBar(backgroundColor: Colors.amber), //!            AppBar
-        body: ListView(
+        body: Stack(
           children: [
-            Column(
+            ListView(
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 15.0, left: 20, right: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    spacing: 20,
-                    children: [
-                      Icon(Icons.circle, size: 60), //!                         Profilbild
-                      Begruessung(name: "Bahri"),
-                      SizedBox(width: 35),
-                      Icon(Icons.pie_chart_sharp, size: 40), //!              Chart-ObenRechts
-                    ],
-                  ),
-                ),
-                SizedBox(height: 10),
-                LeftOverCards(
-                  fach: "Mathe", //!          LeftOverBalken
-                  cardsLeft: "13 Cards left",
-                  fortschrittsBalken: Icons.bar_chart_sharp,
-                  farbe: Colors.pink,
-                ),
-                LeftOverCards(
-                  fach: "Arabisch", //!       LeftOverBalken
-                  cardsLeft: "23 Cards left",
-                  fortschrittsBalken: Icons.bar_chart_sharp,
-                  farbe: Colors.blue,
-                ),
-                BoxErstellen(
-                  fach: [
-                    FachBox(farbe: Colors.pink),
-                    FachBox(farbe: Colors.blue),
-                    FachBox(farbe: Colors.yellow),
-                    FachBox(farbe: Colors.green),
-                    FachBox(farbe: Colors.black),
-                    FachBox(farbe: Colors.purple),
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15.0, left: 20, right: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        spacing: 20,
+                        children: [
+                          Icon(Icons.circle, size: 60), //!                         Profilbild
+                          Begruessung(name: "Bahri"),
+                          SizedBox(width: 35),
+                          Icon(Icons.pie_chart_sharp, size: 40), //!              Chart-ObenRechts
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    LeftOverCards(
+                      fach: "Mathe", //!          LeftOverBalken
+                      cardsLeft: "13 Cards left",
+                      fortschrittsBalken: Icons.bar_chart_sharp,
+                      farbe: Colors.pink,
+                    ),
+                    LeftOverCards(
+                      fach: "Arabisch", //!       LeftOverBalken
+                      cardsLeft: "23 Cards left",
+                      fortschrittsBalken: Icons.bar_chart_sharp,
+                      farbe: Colors.blue,
+                    ),
+                    BoxErstellen(
+                      fach: [
+                        FachBox(farbe: Colors.pink),
+                        FachBox(farbe: Colors.blue),
+                        FachBox(farbe: Colors.yellow),
+                        FachBox(farbe: Colors.green),
+                        FachBox(farbe: Colors.black),
+                        FachBox(farbe: Colors.purple),
+                      ],
+                    ),
                   ],
                 ),
               ],
             ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 30),
+                child: Container(width: 250, height: 60, color: Colors.grey),
+              ),
+            ), //!        BottomMenu
           ],
         ),
       ),
